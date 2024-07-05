1c1bd1d19b1de13071123d113bfc392f61660b3f
Dynezel <dylan50xd@gmail.com>
2024-07-05 14:01:17 -0300

    Modificacion de estilos Mensajes

--- devocionales-app/src/Componentes/Mensajeria.jsx
+++ devocionales-app/src/Componentes/Mensajeria.jsx
@@ -1,30 +1,33 @@
 import React, { useState, useEffect, useRef } from "react";
 import axios from "axios";
 import "../css/Mensajeria.css";
-import Conversaciones from "./Conversaciones";
-import ChatPestaña from "./ChatPestania";
 
-export default function MensajeriaPopup({ usuarioId, usuarioActualId, onClose }) {
+export default function MensajeriaPopup({
+  usuarioId,
+  usuarioActualId,
+  onClose,
+}) {
   const [conversacion, setConversacion] = useState([]);
   const [nuevoMensaje, setNuevoMensaje] = useState("");
   const [minimizado, setMinimizado] = useState(false);
-  const [nombreUsuario, setNombreUsuario] = useState(""); // Nuevo estado para el nombre
-  const [mostrarConversaciones, setMostrarConversaciones] = useState(false); // Nuevo estado para mostrar conversaciones
-  const [usuarioSeleccionado, setUsuarioSeleccionado] = useState(null); // Nuevo estado para manejar el usuario seleccionado
+  const [nombreUsuario, setNombreUsuario] = useState("");
+  const [imagenPerfilUsuario, setImagenPerfilUsuario] = useState(null);
+  const [imagenPerfilOtroUsuario, setImagenPerfilOtroUsuario] = useState(null);
   const popupRef = useRef(null);
-
 
   useEffect(() => {
     const obtenerConversacion = async () => {
       try {
-        const response = await axios.get("http://localhost:8080/mensajes/conversacion", {
-          params: {
-            emisorId: usuarioActualId,
-            receptorId: usuarioId,
-          },
-        });
+        const response = await axios.get(
+          "http://localhost:8080/mensajes/conversacion",
+          {
+            params: {
+              emisorId: usuarioActualId,
+              receptorId: usuarioId,
+            },
+          }
+        );
         setConversacion(response.data);
-        console.log(conversacion)
       } catch (error) {
         console.error("Error al obtener la conversacion:", error);
       }
@@ -32,27 +35,47 @@ export default function MensajeriaPopup({ usuarioId, usuarioActualId, onClose })
 
     const obtenerNombreUsuario = async () => {
       try {
-        const response = await axios.get(`http://localhost:8080/usuario/perfil/${usuarioId}`);
-        setNombreUsuario(response.data.nombre); // Asigna el nombre del usuario al estado
+        const response = await axios.get(
+          `http://localhost:8080/usuario/perfil/${usuarioId}`
+        );
+        setNombreUsuario(response.data.nombre);
       } catch (error) {
         console.error("Error al obtener el nombre del usuario:", error);
       }
     };
 
-    obtenerConversacion();
-    obtenerNombreUsuario(); // Llama a la función para obtener el nombre del usuario
-  }, [usuarioId, usuarioActualId]);
+    const cargarImagenPerfil = async (idUsuario, setImagenPerfil) => {
+      try {
+        const response = await axios.get(
+          `http://localhost:8080/imagen/perfil/${idUsuario}`,
+          { responseType: "arraybuffer" }
+        );
+        const blob = new Blob([response.data], { type: "image/jpeg" });
+        const url = URL.createObjectURL(blob);
+        console.log(`Imagen de perfil URL para el usuario ${idUsuario}:`, url); // Añade esta línea
+        setImagenPerfil(url);
+      } catch (error) {
+        console.error("Error al cargar la imagen de perfil:", error);
+      }
+    };
 
-  
+    obtenerConversacion();
+    obtenerNombreUsuario();
+    cargarImagenPerfil(usuarioActualId, setImagenPerfilUsuario);
+    cargarImagenPerfil(usuarioId, setImagenPerfilOtroUsuario);
+  }, [usuarioId, usuarioActualId]);
 
   const enviarMensaje = async () => {
     if (nuevoMensaje.trim() === "") return;
     try {
-      const response = await axios.post("http://localhost:8080/mensajes/enviar", {
-        emisorId: usuarioActualId,
-        receptorId: usuarioId,
-        contenido: nuevoMensaje,
-      });
+      const response = await axios.post(
+        "http://localhost:8080/mensajes/enviar",
+        {
+          emisorId: usuarioActualId,
+          receptorId: usuarioId,
+          contenido: nuevoMensaje,
+        }
+      );
       setConversacion([...conversacion, response.data]);
       setNuevoMensaje("");
     } catch (error) {
@@ -77,13 +100,6 @@ export default function MensajeriaPopup({ usuarioId, usuarioActualId, onClose })
     }
   };
 
-  const abrirConversaciones = () => setMostrarConversaciones(true);
-
-  const seleccionarConversacion = (usuario) => {
-    setUsuarioSeleccionado(usuario);
-    setMostrarConversaciones(false);
-  };
-
   return (
     <div
       className={`mensajeria-popup ${minimizado ? "minimizado" : ""}`}
@@ -93,36 +109,58 @@ export default function MensajeriaPopup({ usuarioId, usuarioActualId, onClose })
       onDragEnd={dragEnd}
     >
       <div className="popup-header">
-        <span>Mensajes con {nombreUsuario || "Seleccione una conversación"}</span>
-        <button onClick={toggleMinimizado}>{minimizado ? "Expandir" : "Minimizar"}</button>
+        <span>
+          Mensajes con {nombreUsuario || "Seleccione una conversación"}
+        </span>
+        <button onClick={toggleMinimizado}>
+          {minimizado ? "Expandir" : "Minimizar"}
+        </button>
         <button onClick={onClose}>Cerrar</button>
-        <button onClick={abrirConversaciones}>Ver Conversaciones</button>
       </div>
       {!minimizado && (
         <div className="popup-body">
-          {mostrarConversaciones ? (
-            <Conversaciones
-              usuarioActualId={usuarioActualId}
-              seleccionarConversacion={seleccionarConversacion}
+          <>
+            <div className="mensajes">
+              {conversacion.map((mensaje) => (
+                <div
+                  key={mensaje.id}
+                  className={`mensaje ${
+                    mensaje.emisor.idUsuario == usuarioActualId
+                      ? "enviado"
+                      : "recibido"
+                  }`}
+                >
+                  {mensaje.emisor.idUsuario == usuarioActualId ? (
+                    <div className="mensaje-contenido enviado">
+                      <div className="mensaje-texto">{mensaje.contenido}</div>
+                      <img
+                        src={imagenPerfilUsuario}
+                        alt="Tu Imagen de Perfil"
+                        className="profile-picture profile-picture-usuario"
+                      />
+                    </div>
+                  ) : (
+                    <div className="mensaje-contenido recibido">
+                      <img
+                        src={imagenPerfilOtroUsuario}
+                        alt="Imagen de Perfil del Otro Usuario"
+                        className="profile-picture profile-picture-otro"
+                      />
+                      <div className="mensaje-texto">{mensaje.contenido}</div>
+                    </div>
+                  )}
+                </div>
+              ))}
+            </div>
+
+            <input
+              type="text"
+              value={nuevoMensaje}
+              onChange={(e) => setNuevoMensaje(e.target.value)}
+              placeholder="Escribe un mensaje..."
             />
-          ) : (
-            <>
-              <div className="mensajes">
-                {conversacion.map((mensaje) => (
-                  <div key={mensaje.id} className={`mensaje ${mensaje.emisor.id === usuarioActualId ? "enviado" : "recibido"}`}>
-                    <strong>{mensaje.emisor.nombre}:</strong> {mensaje.contenido}
-                  </div>
-                ))}
-              </div>
-              <input
-                type="text"
-                value={nuevoMensaje}
-                onChange={(e) => setNuevoMensaje(e.target.value)}
-                placeholder="Escribe un mensaje..."
-              />
-              <button onClick={enviarMensaje}>Enviar</button>
-            </>
-          )}
+            <button onClick={enviarMensaje}>Enviar</button>
+          </>
         </div>
       )}
     </div>
