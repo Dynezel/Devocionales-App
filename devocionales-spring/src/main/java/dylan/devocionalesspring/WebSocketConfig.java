b4121f7bdf62a8fc8e2ed87e4f14c8577e2fa8e5
Dynezel <dylan50xd@gmail.com>
2025-07-09 10:10:26 -0300

    Preparacion https

--- devocionales-spring/src/main/java/dylan/devocionalesspring/WebSocketConfig.java
+++ devocionales-spring/src/main/java/dylan/devocionalesspring/WebSocketConfig.java
@@ -20,6 +20,6 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
 
     @Override
     public void registerStompEndpoints(StompEndpointRegistry registry) {
-        registry.addEndpoint("/ws-notifications").setAllowedOrigins("https://devocionales-app-frontend.vercel.app").withSockJS(); //En producción, reemplazar setAllowedOrigins("*") por dominios específicos.
+        registry.addEndpoint("/ws-notifications").setAllowedOrigins("https://devocionales-app-frontend.vercel.app").withSockJS();
     }
 }
